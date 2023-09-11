e7f1d25d223c4aefd72ff34758670c982f7f2cdb
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2023-09-11 21:23:48 +0000

    resolved using two pointers

--- top-interview-150/array/Remove_Element.py
+++ top-interview-150/array/Remove_Element.py
@@ -27,6 +27,21 @@ from typing import List
 
 
 class Solution:
+    def removeElement_two_pointers(self, nums: List[int], val: int) -> int:
+        if nums is None or len(nums) == 0:
+            return 0
+        
+        write_idx = 0
+        read_idx = 0
+        
+        while read_idx < len(nums):
+            if nums[read_idx] != val:
+                nums[write_idx] = nums[read_idx]
+                write_idx += 1
+            read_idx += 1
+            
+        return write_idx
+    
     def removeElement(self, nums: List[int], val: int) -> int:
         nums_len = len(nums)
         result = nums_len
@@ -46,25 +61,25 @@ class Solution:
 
 solution = Solution()
 nums = [0,2,2,2,4,5,6]
-print(solution.removeElement(nums, val = 2)) # 4
+print(solution.removeElement_two_pointers(nums, val = 2)) # 4
 print(nums) # [0,4,5,6,_,_,_]
 
 nums = [0,1,2,2,3,0,4,2]
-print(solution.removeElement(nums, val = 2)) # 5
+print(solution.removeElement_two_pointers(nums, val = 2)) # 5
 print(nums) # [0,1,0,4,3,-100,-100,-100]
 
 nums = [3,2,2,3]
-print(solution.removeElement(nums, val = 3)) # 2
+print(solution.removeElement_two_pointers(nums, val = 3)) # 2
 print(nums) # [2,2,_,_]
 
 nums = [0,3,1,1,0,1,3,0,3,3,1,1]
-print(solution.removeElement(nums, val = 1)) # 7
+print(solution.removeElement_two_pointers(nums, val = 1)) # 7
 print(nums) # [0,3,3,3,0,0,3]
 
 nums = [0,1,1,1,1,1,1,1,1,1,1]
-print(solution.removeElement(nums, val = 1)) # 1
+print(solution.removeElement_two_pointers(nums, val = 1)) # 1
 print(nums) # [0,_,....]
 
 nums = [1,1,1,1,1,1,1,1,1,1,0]
-print(solution.removeElement(nums, val = 1)) # 1
+print(solution.removeElement_two_pointers(nums, val = 1)) # 1
 print(nums) # [0,_,....]
