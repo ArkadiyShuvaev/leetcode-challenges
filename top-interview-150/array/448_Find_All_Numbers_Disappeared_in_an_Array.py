d252080a6ba21e1d5be1609fde0c0d723754806a
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2023-12-10 19:59:09 +0000

    solved with compute O(n) and memory O(1)

--- top-interview-150/array/448_Find_All_Numbers_Disappeared_in_an_Array.py
+++ top-interview-150/array/448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -36,33 +36,39 @@ class Solution:
 
         return list(all_numbers_set)
 
-    # This method has O(n) complexity with extra memory (array)
+    # This method has O(n) complexity with O(1) memory (It is assumed the returned list does not count as extra space)
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         nums_len = len(nums)
         if nums_len == 1:
             return []
 
-        nums.sort()
         disappeared_nums = []
-        previous_val = 0
 
+        # Iterate through the array and mark the value at the current index as negative
+        # if the value at the current index is positive.
+        # If the value at the current index is negative, then it is already marked.
+        for item in nums:
+            idx = abs(item) - 1
+            idx_value = nums[idx]
+            if idx_value > 0:
+                nums[idx] = -idx_value
+
+        # Iterate through the array and add the index of the values that are positive
+        # to the final result. The index of the values that are positive represent
+        # the numbers that never appeared in the array.
         for i in range(0, nums_len):
-            current_value = nums[i]
-            if current_value - previous_val > 1:
-                for i in range(previous_val + 1, current_value):
-                    disappeared_nums.append(i)
-
-            previous_val = current_value
+            if nums[i] > 0:
+                disappeared_nums.append(i + 1)
 
         return disappeared_nums
 
 solution = Solution()
 
 
-assert solution.findDisappearedNumbers([1,1,1]) == [2,3]
 assert solution.findDisappearedNumbers([4,3,2,7,8,2,3,1]) == [5, 6]
+assert solution.findDisappearedNumbers([1,1,1]) == [2,3]
 assert solution.findDisappearedNumbers([4,3,2,7,8,2,3,2]) == [1, 5, 6] # [2, 2, 2, 3, 3, 4, 7, 8]
 assert solution.findDisappearedNumbers([4,3,2,7,7,2,3,1]) == [5, 6, 8]
-assert solution.findDisappearedNumbers([3,2]) == [1]
+assert solution.findDisappearedNumbers([3,2, 3]) == [1]
 assert solution.findDisappearedNumbers([1,1]) == [2]
 assert solution.findDisappearedNumbers([1]) == []
