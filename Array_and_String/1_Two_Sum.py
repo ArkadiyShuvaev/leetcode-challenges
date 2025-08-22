49ae757d18a37bae0ee9d7776475304ad2615312
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2025-08-22 11:41:11 +0000

    optimized task 1: add dictionary

--- Array_and_String/1_Two_Sum.py
+++ Array_and_String/1_Two_Sum.py
@@ -31,23 +31,28 @@
 
 from typing import List
 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         #        [2,7,11,15], 9
         # Index:  0 1  2  3
+        index_of = {}
 
-        for external_idx in range(len(nums) - 1):
-            for internal_idx in range(external_idx + 1, len(nums)):
-              items_sum = nums[external_idx] + nums[internal_idx]
-              if items_sum == target:
-                  return [external_idx, internal_idx]
+        for pointer_idx in range(len(nums) - 1):
+            index_of[nums[pointer_idx]] = pointer_idx
+            next_pointer_idx = pointer_idx + 1
+            required_number = target - nums[next_pointer_idx]
+
+            if required_number in index_of:
+                return [index_of[required_number], next_pointer_idx]
 
         return []
 
+
 solution = Solution()
 
-assert solution.twoSum([2,7,11,15], 9) == [0,1]
-assert solution.twoSum([3,2,4], 6) == [1,2]
-assert solution.twoSum([3,3], 6) == [0, 1]
-assert solution.twoSum([3,2,3], 6) == [0, 2]
-assert solution.twoSum([2,5,5,11], 10) == [1, 2]
+assert solution.twoSum([2, 5, 5, 11], 10) == [1, 2]
+assert solution.twoSum([2, 7, 11, 15], 9) == [0, 1]
+assert solution.twoSum([3, 2, 4], 6) == [1, 2]
+assert solution.twoSum([3, 3], 6) == [0, 1]
+assert solution.twoSum([3, 2, 3], 6) == [0, 2]
