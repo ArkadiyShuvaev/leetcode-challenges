3aeb096b50b7b423b70fb62460a0b648e01f95b0
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2025-08-26 14:20:13 +0000

    36. Valid Sudoku with O(81)

---
+++ top-interview-150/array/36_Valid_Sudoku.js
@@ -0,0 +1,143 @@
+// 36. Valid Sudoku
+//
+// Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+//
+// 1. Each row must contain the digits 1-9 without repetition.
+// 2. Each column must contain the digits 1-9 without repetition.
+// 3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+//
+// Note:
+// A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+// Only the filled cells need to be validated according to the mentioned rules.
+//
+// Example 1:
+// Input: board =
+// [["5","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
+// Output: true
+//
+//
+// Example 2:
+// Input: board =
+// [["8","3",".",".","7",".",".",".","."]
+// ,["6",".",".","1","9","5",".",".","."]
+// ,[".","9","8",".",".",".",".","6","."]
+// ,["8",".",".",".","6",".",".",".","3"]
+// ,["4",".",".","8",".","3",".",".","1"]
+// ,["7",".",".",".","2",".",".",".","6"]
+// ,[".","6",".",".",".",".","2","8","."]
+// ,[".",".",".","4","1","9",".",".","5"]
+// ,[".",".",".",".","8",".",".","7","9"]]
+// Output: false
+//
+// Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
+// Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+//
+//
+// Constraints:
+// board.length == 9
+// board[i].length == 9
+// board[i][j] is a digit 1-9 or '.'.
+
+/**
+ * @param {string[][]} board
+ * @return {boolean}
+ * The solution with O(81) time
+ */
+var isValidSudoku = function (board) {
+    // row-{number}
+    // col-{number}
+    //
+
+    const set = new Set();
+
+    for (let rowIdx = 0; rowIdx < board.length; rowIdx++) {
+        for (let colIdx = 0; colIdx < board.length; colIdx++) {
+            const number = board[rowIdx][colIdx]
+            if (number === ".") {
+                continue
+            }
+
+            const rowElem = `row-${rowIdx}-${number}`;
+            const colElem = `col-${colIdx}-${number}`;
+
+            const boxRowId = Math.floor(rowIdx/3);
+            const boxColId = Math.floor(colIdx/3);
+            const boxElem = `box-${boxRowId}-${boxColId}-${number}`;
+
+            if (set.has(rowElem) || set.has(colElem) || set.has(boxElem)) {
+                return false;
+            }
+
+            set.add(rowElem);
+            set.add(colElem);
+            set.add(boxElem);
+        }
+    }
+
+    return true;
+};
+
+// [2][2] 2 % 3 = 1
+// [8][8] 8 % 3 = 2; 8 / 3 = 2
+
+let board = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+
+console.assert(isValidSudoku(board) === true, "Test case 1 failed");
+
+board = [
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+
+console.assert(isValidSudoku(board) === false, "Test case 2 failed");
+
+board = [
+    ["5", "3", ".", ".", "5", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+console.assert(isValidSudoku(board) === false, "Test case 3 failed");
+
+board = [
+    [".", ".", ".", ".", "5", ".", ".", "1", "."],
+    [".", "4", ".", "3", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", "3", ".", ".", "1"],
+    ["8", ".", ".", ".", ".", ".", ".", "2", "."],
+    [".", ".", "2", ".", "7", ".", ".", ".", "."],
+    [".", "1", "5", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", "2", ".", ".", "."],
+    [".", "2", ".", "9", ".", ".", ".", ".", "."],
+    [".", ".", "4", ".", ".", ".", ".", ".", "."]
+]
+console.assert(isValidSudoku(board) === false, "Test case 4 failed");
