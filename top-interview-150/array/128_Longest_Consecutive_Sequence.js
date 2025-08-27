e3e0dd2bb75ccc9201a5b467765d1215ca912e99
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2025-08-27 09:42:51 +0000

    solved 128. Longest Consecutive Sequence with O(n) time

--- top-interview-150/array/128_Longest_Consecutive_Sequence.js
+++ top-interview-150/array/128_Longest_Consecutive_Sequence.js
@@ -26,31 +26,23 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-    // [100,4,200,1,3,2] === 4
-    // { 100,4,200,1,3,2 }
-    // -109, -108, -107, 1, ..... 108, 109
+    const set = new Set(nums);
 
-    const set = new Set();
     let longestSecuence = 0;
-
-    for (let i = 0; i < nums.length; i++) {
-        set.add(nums[i]);
-    }
-    // 100 -> longestSecuence: 1
-    // 1 -> longestSecuence: 1, longestSecuence: 2
     let currentSecuenceLength = 0;
-    for (let number = -109; number <= 109; number++) {
-        if (set.has(number)) {
-            currentSecuenceLength += 1;
 
-            if (currentSecuenceLength > longestSecuence) {
-                longestSecuence = currentSecuenceLength;
+    for (const number of set) {
+        // Only start when there is no predecessor
+        if (!set.has(number - 1)) {
+            let startNumber = number;
+
+            while (set.has(startNumber++)) {
+                currentSecuenceLength++;
             }
 
-            continue;
+            longestSecuence = Math.max(longestSecuence, currentSecuenceLength);
+            currentSecuenceLength = 0;
         }
-
-        currentSecuenceLength = 0;
     }
 
     return longestSecuence;
@@ -61,3 +53,10 @@ console.assert(longestConsecutive([0,3,7,2,5,8,4,6,0,1]) === 9, "Test 2 failed")
 console.assert(longestConsecutive([1,0,1,2]) === 3, "Test 3 failed");
 console.assert(longestConsecutive([-100, -101, -1,0,1,2, 100, 101]) === 4, "Test 4 failed");
 console.assert(longestConsecutive([]) === 0, "Test 5 failed");
+
+const startDt = new Date();
+console.assert(longestConsecutive([0,1,2,4,8,5,6,7,9,3,55,88,77,99,999999999]) === 10)
+const stopDt = new Date();
+
+const diff = (stopDt.getTime() - startDt.getTime());
+console.info(`Diff in seconds: ${diff/1000}`);
